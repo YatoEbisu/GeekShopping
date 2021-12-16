65bd7e03aa41c797267fc79396d1487f83cbf6f4
Breno SF <BrenoSilvaFortunato@gmail.com>
2021-12-16 10:01:32 -0300

    :sparkles: Product API

---
+++ GeekShopping.ProductAPI/Controllers/ProductController.cs
@@ -0,0 +1,58 @@
+﻿using GeekShopping.ProductAPI.Data.ValueObjects;
+using GeekShopping.ProductAPI.Repository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GeekShopping.ProductAPI.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private IProductRepository _repository;
+
+        public ProductController(IProductRepository repository)
+        {
+            _repository = repository;
+        }
+        [HttpGet]
+        public async Task<IActionResult> FindAll()
+        {
+            var products = await _repository.FindAll();
+            return Ok(products);
+        }
+
+        [HttpGet("{id:long}")]
+        public async Task<ActionResult> FindById(long id)
+        {
+            var product = await _repository.FindById(id);
+            if (product == null) return NotFound();
+            return Ok(product);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<ProductVO>> Create(ProductVO productVO)
+        {
+            if(productVO == null) return BadRequest();
+            var product = await _repository.Create(productVO);
+            return Created(nameof(Create), product);
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<ProductVO>> Update(ProductVO productVO)
+        {
+            if(productVO == null) return BadRequest();
+            var product = await _repository.Update(productVO);
+            return Ok(product);
+        }
+
+        [HttpDelete("{id:long}")]
+        public async Task<ActionResult> Delete(long id)
+        {
+            var status = await _repository.Delete(id);
+            if(!status) return BadRequest();
+            return Ok(status);
+
+        }
+    }
+}
