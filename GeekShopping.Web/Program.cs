4d8e2a8ba1f6d35c99c196b9302d3a5938a98404
Breno SF <BrenoSilvaFortunato@gmail.com>
2021-12-27 14:21:59 -0300

    DI httpclient

--- GeekShopping.Web/Program.cs
+++ GeekShopping.Web/Program.cs
@@ -1,7 +1,14 @@
+using GeekShopping.Web.Services;
+using GeekShopping.Web.Services.IServices;
+
 var builder = WebApplication.CreateBuilder(args);
+
+ConfigurationManager configuration = builder.Configuration;
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddHttpClient<IProductService, ProductService>(c => c
+    .BaseAddress = new Uri(configuration["ServicesUrls:ProductAPI"]));
 
 var app = builder.Build();
 
